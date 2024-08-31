c7d972acb55f73672390a49d0967adf822578fc5
Deivid de Jesus <imdeividsilva@gmail.com>
2024-08-31 14:55:44 -0300

    criação do componente BotaoContainer.jsx

---
+++ src/components/ComponentesComuns/Financeiro/BotaoContainer.jsx
@@ -0,0 +1,7 @@
+export const BotaoContainer = ({ children }) => {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            {children}
+        </div>
+    )
+}
